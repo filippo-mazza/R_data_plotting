73bd9161fc17fbf3958281958ec3ea325bd00217
Filippo Mazza - coding <mazza.networking@gmail.com>
2017-10-29 18:09:11 +0100

    Added scatter by col and faceted

--- R graphs.R
+++ R graphs.R
@@ -5,6 +5,10 @@ library(ggplot2)
 #diamond data set is in ggplot2
 GGally::ggpairs(diamonds[1:10])
 
+#The prices have a strong long tail distribution
+qplot(diamonds$price)
+
+
 #Checking the prices distribution by cut and color
 ggplot(data=diamonds, aes(x=price))+geom_histogram()+facet_grid(diamonds$cut ~ diamonds$color)
 
@@ -15,3 +19,11 @@ ggplot(data=diamonds, aes(y=price,x=table,colour=cut)) + geom_point() + scale_co
 diamonds$volume = diamonds$x * diamonds$y * diamonds$z
 tmp <- diamonds[diamonds$volume<quantile(diamonds$volume,c(.99)),]
 ggplot(data=tmp, aes(y=price,x=volume,colour=clarity)) + geom_point() + scale_color_brewer(type = 'div') + scale_y_log10()
+
+
+ggplot(diamonds,aes(table,price))+stat_summary(fun.y=median, geom="line")
+
+
+#Scatter plot of price/carat vs cut by clarity + col by ... color
+ggplot(diamonds, aes(x=cut, y=price/carat,colour=color)) +
+  geom_point(size=2) + facet_grid(diamonds$clarity ~.)
